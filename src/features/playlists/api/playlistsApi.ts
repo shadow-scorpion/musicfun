2c84cde570fa5b0b9de7982201a890c509acf256
Alex <secret.teamplayer@gmail.com>
2026-04-01 17:48:37 +0300

    feat: add RTK Query tags for cache invalidation

--- src/features/playlists/api/playlistsApi.ts
+++ src/features/playlists/api/playlistsApi.ts
@@ -13,18 +13,23 @@ export const playlistsApi = createApi({
       return headers;
     },
   }),
+  tagTypes: ['Playlists'],
   endpoints: (build) => ({
     getPlaylists: build.query<PlaylistsResponse, void>({
       query: () => ({ method: 'get', url: 'playlists' }),
+      providesTags: ['Playlists'],
     }),
     createPlaylist: build.mutation<PlaylistsResponse, CreatePlaylistArg>({
       query: (body) => ({ method: 'post', url: 'playlists', body }),
+      invalidatesTags: ['Playlists'],
     }),
     removePlaylist: build.mutation<void, string>({
       query: (playlistId) => ({ method: 'delete', url: `playlists/${playlistId}` }),
+      invalidatesTags: ['Playlists'],
     }),
     updatePlaylist: build.mutation<void, { playlistId: string; body: UpdatePlaylistArg }>({
       query: ({ playlistId, body }) => ({ method: 'put', url: `playlists${playlistId}`, body }),
+      invalidatesTags: ['Playlists'],
     }),
   }),
 });

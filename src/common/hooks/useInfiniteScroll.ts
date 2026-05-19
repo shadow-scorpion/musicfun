71eebe1b85a2456e97f20b8b32b625180c6964de
Alex <secret.teamplayer@gmail.com>
2026-05-19 22:58:11 +0300

    fear: add common hook infinite scroll

---
+++ src/common/hooks/useInfiniteScroll.ts
@@ -0,0 +1,40 @@
+import { useCallback, useEffect, useRef } from 'react';
+
+type Props = {
+  hasNextPage: boolean;
+  isFetching: boolean;
+  fetchNextPage: () => void;
+};
+
+export const useInfiniteScroll = ({ hasNextPage, isFetching, fetchNextPage }: Props) => {
+  const observerRef = useRef<HTMLDivElement>(null);
+
+  const loadTracksHandler = useCallback(() => {
+    if (hasNextPage && !isFetching) {
+      fetchNextPage();
+    }
+  }, [hasNextPage, isFetching, fetchNextPage]);
+
+  useEffect(() => {
+    const observer = new IntersectionObserver(
+      (entries) => {
+        const [entry] = entries;
+        if (entry && entry.isIntersecting) {
+          loadTracksHandler();
+        }
+      },
+      { rootMargin: '50px' },
+    );
+    const currentObservableRef = observerRef.current;
+    if (currentObservableRef) {
+      observer.observe(currentObservableRef);
+    }
+
+    return () => {
+      if (currentObservableRef) {
+        observer.unobserve(currentObservableRef);
+      }
+    };
+  }, [loadTracksHandler, observerRef]);
+  return { observerRef };
+};

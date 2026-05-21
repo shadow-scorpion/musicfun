4a51a7e18975039fb3165671ea663723d421713b
Alex <secret.teamplayer@gmail.com>
2026-05-21 19:07:48 +0300

    feat: add global loading progress line with logic
    - add custom <LinearProgress/> component and styles
    - add common hook useGlobalLoading
    - use it to <App/>

--- src/app/ui/App/App.tsx
+++ src/app/ui/App/App.tsx
@@ -2,11 +2,16 @@ import { Routing } from '@/common/routing/Routing.tsx';
 import { Header } from '@/widgets/Header/Header.tsx';
 import s from './App.module.css';
 import { ToastContainer } from 'react-toastify';
+import { LinearProgress } from '@/common/components/LinearProgress/LinearProgress.tsx';
+import { useGlobalLoading } from '@/common/hooks/useGlobalLoading.tsx';
 
 export function App() {
+  const isGlobalLoading = useGlobalLoading();
+
   return (
     <>
       <Header />
+      {isGlobalLoading && <LinearProgress />}
       <div className={s.layout}>
         <Routing />
       </div>

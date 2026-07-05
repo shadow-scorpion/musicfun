9fcfc6dc47c5b04d5c317cff614043b7141ca492
Alex <secret.teamplayer@gmail.com>
2026-07-05 19:36:20 +0300

    feat: create api requests in authApi login, logout, authRefresh

--- src/features/auth/api/authApi.ts
+++ src/features/auth/api/authApi.ts
@@ -1,12 +1,45 @@
 import { baseApi } from '@/app/api/baseApi.ts';
-import type { MeResponse } from '@/features/auth/api/authApi.types.ts';
+import type { LoginArgs, LoginResponse, MeResponse } from '@/features/auth/api/authApi.types.ts';
+import { AUTH_STORAGE_KEY } from '@/common/constants';
 
 export const authApi = baseApi.injectEndpoints({
   endpoints: (build) => ({
     getMe: build.query<MeResponse, void>({
       query: () => ({ method: 'get', url: '/auth/me' }),
+      providesTags: ['Auth'],
+    }),
+    login: build.mutation<LoginResponse, LoginArgs>({
+      query: (payload) => ({
+        url: '/auth/login',
+        method: 'post',
+        body: { ...payload, accessTokenTTL: '10m' },
+      }),
+      onQueryStarted: async (_args, { queryFulfilled, dispatch }) => {
+        const { data } = await queryFulfilled;
+        localStorage.setItem(AUTH_STORAGE_KEY.accessToken, data.accessToken);
+        localStorage.setItem(AUTH_STORAGE_KEY.refreshToken, data.refreshToken);
+
+        dispatch(authApi.util.invalidateTags(['Auth']));
+      },
+    }),
+    logout: build.mutation<void, void>({
+      query: () => {
+        const refreshToken = localStorage.getItem(AUTH_STORAGE_KEY.refreshToken);
+        return { method: 'post', url: '/auth/logout', body: { refreshToken } };
+      },
+      onQueryStarted: async (_args, { queryFulfilled, dispatch }) => {
+        await queryFulfilled;
+        localStorage.removeItem(AUTH_STORAGE_KEY.accessToken);
+        localStorage.removeItem(AUTH_STORAGE_KEY.refreshToken);
+        dispatch(authApi.util.resetApiState());
+      },
+    }),
+    authRefresh: build.mutation<LoginResponse, { refreshToken: string }>({
+      query: (body) => {
+        return { method: 'post', url: '/auth/refresh', body };
+      },
     }),
   }),
 });
 
-export const { useGetMeQuery } = authApi;
+export const { useGetMeQuery, useLoginMutation, useLogoutMutation, useAuthRefreshMutation } = authApi;
--- src/features/auth/api/authApi.types.ts
+++ src/features/auth/api/authApi.types.ts
@@ -2,3 +2,15 @@ export type MeResponse = {
   userId: string;
   login: string;
 };
+
+export type LoginArgs = {
+  code: string;
+  redirectUri: string;
+  accessTokenTTL?: string;
+  rememberMe: boolean;
+};
+
+export type LoginResponse = {
+  refreshToken: string;
+  accessToken: string;
+};

c3a03712f18086450c71f86150b1736e9f21c516
Alex <secret.teamplayer@gmail.com>
2026-07-20 17:26:03 +0300

    refactor: move CreatePlaylistForm.tsx to ProfilePage.tsx
    -move CreatePlaylistForm.tsx and CreatePlaylistForm.module.css

--- src/features/auth/ui/ProfilePage/ProfilePage.tsx
+++ src/features/auth/ui/ProfilePage/ProfilePage.tsx
@@ -1,4 +1,5 @@
 import { useGetMeQuery } from '@/features/auth/api/authApi.ts';
+import { CreatePlaylistForm } from '@/features/auth/ui/ProfilePage/CreatePlaylistForm/CreatePlaylistForm.tsx';
 
 export const ProfilePage = () => {
   const { data: meData } = useGetMeQuery();
@@ -6,6 +7,8 @@ export const ProfilePage = () => {
   return (
     <div>
       <h1>You are {meData?.login} </h1>
+      <CreatePlaylistForm />
+      <PlaylistsList playlist={playlistsData?.data || []} isLoading={isMeLoading || isLoading} />
     </div>
   );
 };
